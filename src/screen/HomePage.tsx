b333444d41a29d55f9ce7d84c836ef6b5446cc71
Carloslt <carloslt.proyectos@gmail.com>
2023-11-29 17:51:19 +0100

    homepage redirect signin

--- src/screen/HomePage.tsx
+++ src/screen/HomePage.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { StyledButton } from '../styles/StyledButton'
+import StyledText from '../styles/StyledText'
+import { useNavigate } from 'react-router-native'
 
 const HomePage = () => {
+  const navigate = useNavigate()
+
+  const handleSignInPress = () => {
+    navigate('/signin')
+  }
+
   return (
-    <View>HomePage</View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Rate Repositories</Text>
+      <StyledButton style={{ width: '50%' }} onPress={handleSignInPress}>
+        <StyledText
+          color='textWhite'
+          fontWeight='bold'
+          align='center'
+        >
+          Sign In
+        </StyledText>
+      </StyledButton >
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginBottom: 50,
+    textAlign: 'center'
+  }
+})
 
 export default HomePage
